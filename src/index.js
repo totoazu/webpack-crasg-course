16b98703b6cfd7c5f400550cf1ca92bce713ab49
内田拓磨 <uchida@wetch.co.jp>
2019-06-27 20:06:36 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 function component(){
   const element = document.createElement("div");
-  const array = ["Hello", "webpack"];
+  const array = ["Hello", "webpack", "よっこらせ"];
   element.innerHTML = _.join(array,' ');
   return element;
 }
